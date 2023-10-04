2aa2dca11f89fdd539d2ea1467d5deb75328ee2e
Node Army <109174478+sipalingnode@users.noreply.github.com>
2023-10-04 20:12:54 +0700

    Update sqd.sh

--- sqd.sh
+++ sqd.sh
@@ -17,21 +17,11 @@ echo -e "\e[0m"
 sleep 2
 
 echo -e "\e[1m\e[32m Install Node.js, Docker, Git \e[0m" && sleep 1
-sudo apt update
-
-sudo apt install git
+sudo apt-get update && sudo apt install git -y && sudo apt install apt-transport-https ca-certificates curl software-properties-common -y && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" && sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
 sudo apt install nodejs
 
 sudo apt install npm
 npm install -g npm@10.2.0
-
-sudo apt-get install ca-certificates curl gnupg lsb-release -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-
-docker compose up -d
 
 echo '=============== DONE ==================='
